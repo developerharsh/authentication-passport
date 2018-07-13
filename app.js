1116987e7893d0704bfeab116e74e4db55b695a8
developerharsh <harshchauhan880@gmail.com>
2018-07-14 01:30:15 +0530

    register page done

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
 var express = require("express");
 var mongoose = require("mongoose");
-
+var passport = require("passport");
+var bodyParser = require("body-parser");
+var localStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+var User= require("./models/user");
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app=express();
 
+app.use(require("express-session")({
+    secret: "dogs are best",
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.set('view engine','ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({extended:true}));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res){
     res.render("home");
 });
 
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
 app.get("/secret",function(req,res){
     res.render("secret");
+});
+
+app.post("/register",function(req,res){
+    User.register(new User({username: req.body.username}),req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }
+
+        passport.authenticate("local")(req,res,function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
 });
 
 app.listen(3000,function(){
